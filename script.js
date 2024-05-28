34cbd4fa872b343c72f09b1b3cc7e05373747acc
matthew rendely <matthew.rendely@gmail.com>
2024-05-27 20:24:43 -0700

    Cleanup and add more filters

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-let elements = document.querySelectorAll('*');
-let arias = Array.from(elements).filter(element => {
-return ((Array.from(element.attributes).some(attr => attr.name.startsWith('aria-label')) && element.ariaHidden !== 'true') && isElementInViewport(element) || ['a','input','button'].includes(element.tagName.toLowerCase()) && isElementInViewport(element)) ;
-});
-
-let ariasresult = arias.map(a => ({tag: a.nodeName, name: a.name, type:a.type,  label: a.ariaLabel, text: a.innerText}))
-
-console.log(ariasresult);
-
-function isElementInViewport (el) {
+function isElementInViewport(el) {
 
     // Special bonus for those using jQuery
     if (typeof jQuery === "function" && el instanceof jQuery) {
@@ -19,7 +10,31 @@ function isElementInViewport (el) {
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
+
+let elements = document.querySelectorAll('*');
+let arias = Array.from(elements).filter(element => {
+    return (
+        
+            (
+                Array.from(element.attributes).some(attr => attr.name.startsWith('aria-label'))
+
+                || 
+                
+                ['a', 'input', 'button'].includes(element.tagName.toLowerCase())
+            )
+            && element.ariaHidden !== 'true'
+            && isElementInViewport(element)
+            && element.checkVisibility()
+
+        )});
+
+let ariasresult = arias.map(a => ({ tag: a.nodeName, hidden: a.ariaHidden, name: a.name, type: a.type, title: a.title, label: a.ariaLabel, text: a.innerText, rect: a.getBoundingClientRect(), href: a.href }));
+
+ariasresult
+
+console.table(ariasresult);
+
